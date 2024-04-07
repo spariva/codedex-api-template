0decdae10a2023f15af0ae2fba0a0d1068b36877
Maki <makspariva@gmail.com>
2024-04-07 02:08:42 +0200

    Search albums function
    Now I am going to experiment to see if I can use different functions, depending on waht results I take. For example if I search Aurora, it should give me back the artist, but also playlists? How Spotify does it?

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,71 @@
 
 import './App.css'
 import { useState, useEffect } from "react";
-import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
+import { FormControl, InputGroup, Container, Button, Row, Card } from "react-bootstrap";
 
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
-const [searchInput, setSearchInput] = useState("");
-const [accessToken, setAccessToken] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  //const [artistID, setArtistID] = useState(""); No entiendo por qué este no lo necesito.
+  const [albums, setAlbums] = useState([]);
 
-useEffect(() => {
-  let authParams = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body:
-      "grant_type=client_credentials&client_id=" +
-      clientId +
-      "&client_secret=" +
-      clientSecret,
-  };
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+    };
 
-  fetch("https://accounts.spotify.com/api/token", authParams)
-    .then((result) => result.json())
-    .then((data) => {
-      setAccessToken(data.access_token);
-    });
-}, []);
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
 
-async function search() {
-  let artistParams = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + accessToken,
-    },
-  };
+  async function search() {
+    let artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
 
-  // Get Artist
-  const artistID = await fetch(
-    "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
-    artistParams
-  )
-    .then((result) => result.json())
-    .then((data) => {
-      return data.artists.items[0].id;
-    });
+    // Get Artist
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
 
-    console.log("Id artista: " + artistID);
-    console.log("Search input: " + searchInput);
-}
-
+    // Get Artist Albums
+    await fetch(
+      "https://api.spotify.com/v1/artists/" +
+      artistID +
+      "/albums?include_groups=album&market=US&limit=50",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        setAlbums(data.items);
+      });
+  }
 
   return (
     <>
-      <h1>Pruebas API</h1>
       <Container>
         <InputGroup>
           <FormControl
@@ -67,8 +76,8 @@ async function search() {
               if (event.key === "Enter") {
                 search();
               }
-            }} // search function
-            onChange={(event) => setSearchInput(event.target.value)} // setSearch
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
             style={{
               width: "300px",
               height: "35px",
@@ -79,15 +88,82 @@ async function search() {
               paddingLeft: "10px",
             }}
           />
-
           <Button onClick={search}>Search</Button>
         </InputGroup>
       </Container>
+
+      <Container>
+        <Row
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            alignContent: "center",
+          }}
+        >
+          {albums.map((album) => {
+            return (
+              <Card
+                key={album.id}
+                style={{
+                  backgroundColor: "white",
+                  margin: "10px",
+                  borderRadius: "5px",
+                  marginBottom: "30px",
+                }}
+              >
+                <Card.Img
+                  width={200}
+                  src={album.images[0].url}
+                  style={{
+                    borderRadius: "4%",
+                  }}
+                />
+                <Card.Body>
+                  <Card.Title
+                    style={{
+                      whiteSpace: "wrap",
+                      fontWeight: "bold",
+                      maxWidth: "200px",
+                      fontSize: "18px",
+                      marginTop: "10px",
+                      color: "black",
+                    }}
+                  >
+                    {album.name}
+                  </Card.Title>
+                  <Card.Text
+                    style={{
+                      color: "black",
+                    }}
+                  >
+                    Release Date: <br /> {album.release_date}
+                  </Card.Text>
+                  <Button
+                    href={album.external_urls.spotify}
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      fontWeight: "bold",
+                      fontSize: "15px",
+                      borderRadius: "5px",
+                      padding: "10px",
+                    }}
+                  >
+                    Album Link
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // A Container to wrap around the search box.
 // An InputGroup used as the form for our search box.
